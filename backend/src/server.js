8f01731d780cb345496b9ef887edb86b0c6f5716
Akshay Kathwate <akshaykathwate14@gmail.com>
2025-11-10 15:20:11 +0530

    Code Ready for Deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,26 @@
 import express from "express";
 import {ENV} from "./lib/env.js";
+import path from "path";
 
 const app = express();
+const __dirname = path.resolve();
 
-app.get("/", (req, res) => {
-  res.status(200).json({ msg: "api called" });
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
 });
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
+app.get("/books", (req, res) => {
+  res.status(200).json({ msg: "this is the books endpoint" });
+});
 
-app.listen(3000, () => console.log("server is running on port 3000"));
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(ENV.PORT || 4000,()=>{
+  console.log(`server started on ${ENV.PORT}`);
+})
